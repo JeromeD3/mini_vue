34725829770af7ec8a8b9294ab95286a46a4df7b
邓祥柏 <1760399176@qq.com>
2023-03-08 00:27:39 +0800

    🚀 feat: Implement the reference type of ref

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,18 +1,33 @@
-import { hasChanged } from '@mini_vue/shared'
+import { hasChanged, isObject } from '@mini_vue/shared'
 import { isTracking, trackEffects, triggerEffects } from './effect'
+import { reactive } from './reactive'
+
+/*
+*  基本类型是通过ref
+*  对象类型是通过reactive
+*  基本类型没有get和set，所以通过class来实现它的get和set
+*/
 class RefImpl {
+  // 用于返回的响应式对象
   private _value: any
+  // 用于对比的原始值
+  private _rawValue: any
   // 将effect收集到的依赖存起来
   public dep: Set<unknown>
 
   constructor(value) {
-    this._value = value
+    // 保存原始值，后续get操作会将value转成响应式对象，
+    // 所以要保存原始值用于set操作中的hasChanged的对比
+    this._rawValue = value
+
+    // 如果是对象的话，需要 value -> reactive
+    this._value = convert(value)
+
     this.dep = new Set()
   }
 
   get value() {
     trackRefValue(this)
-
     return this._value
   }
 
@@ -25,11 +40,16 @@ class RefImpl {
     //   dummy = a.value
     // })
 
-    if (hasChanged(this._value, newValue)) {
-      this._value = newValue
+    if (hasChanged(this._rawValue, newValue)) {
+      this._rawValue = newValue
+      this._value = convert(newValue)
       triggerEffects(this.dep)
     }
   }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value
 }
 
 function trackRefValue(ref) {
