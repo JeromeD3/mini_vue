57db20a372dde43e5757e5531f6b1c5066f35e6e
邓祥柏 <1760399176@qq.com>
2023-03-08 22:37:05 +0800

    🚀 feat: Initialize the main process of component

---
+++ packages/runtime-core/src/component.ts
@@ -0,0 +1,40 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  }
+  return component
+}
+
+export function setupComponent(instance) {
+  // initProps()
+  // initSlots()
+
+  setupStatefulComponent(instance)
+}
+
+function setupStatefulComponent(instance: any) {
+  const Component = instance.vnode.type
+  const { setup } = Component
+
+  if (setup) {
+    // function object
+    const setupResult = setup()
+    handleSetupResult(instance, setupResult)
+  }
+}
+
+function handleSetupResult(instance, setupResult: any) {
+  if (typeof setupResult === 'object') {
+    // state
+    instance.setupState = setupResult
+  }
+  finishComponentSetup(instance)
+}
+
+function finishComponentSetup(instance: any) {
+  const Component = instance.type
+  if (Component.render) {
+    instance.render = Component.render
+  }
+}
---
+++ packages/runtime-core/src/renderer.ts
@@ -0,0 +1,32 @@
+import { createComponentInstance, setupComponent } from './component'
+
+export function render(vnode, container) {
+  // patch
+  patch(vnode, container)
+}
+
+function patch(vnode, container) {
+  // 1. 判断新旧vnode是否是同一个对象
+  // 2. 如果是同一个对象，那么就是更新操作
+  // 3. 如果不是同一个对象，那么就是替换操作
+
+  // 去处理组件
+
+  // 判断 是不是element类型
+  processComponent(vnode, container)
+}
+
+function processComponent(vnode, container) {
+  mountComponent(vnode, container)
+}
+function mountComponent(vnode, container) {
+  const instance = createComponentInstance(vnode)
+  setupComponent(instance)
+  setupRenderEffect(instance, container)
+}
+function setupRenderEffect(instance: any , container: any) {
+  const subTree = instance.render()
+  // vnode -> patch
+  // vnode -> element -> mountElement
+  patch(subTree, container)
+}
