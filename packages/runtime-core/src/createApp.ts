1fbd2fedbab8db81e3ade99a714d757419f513e5
邓祥柏 <1760399176@qq.com>
2023-03-13 16:53:21 +0800

    🚀 feat:  Implement element initialization

--- packages/runtime-core/src/createApp.ts
+++ packages/runtime-core/src/createApp.ts
@@ -3,6 +3,7 @@ import { createVNode } from './vnode'
 
 export function createApp(rootComponent) {
   return {
+    // 因为这里首次传过来的是一个rootComponent，他的type是一个对象，所以一开始会执行processComponent
     // example: app.mount('#app')
     mount(rootContainer) {
       // 会先基于根容器 创建vnode
--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1,4 +1,5 @@
 import { createComponentInstance, setupComponent } from './component'
+import { isObject } from '../../shared/src/index'
 
 export function render(vnode, container) {
   // patch
@@ -9,24 +10,71 @@ function patch(vnode, container) {
   // 1. 判断新旧vnode是否是同一个对象
   // 2. 如果是同一个对象，那么就是更新操作
   // 3. 如果不是同一个对象，那么就是替换操作
-
-  // 去处理组件
-
-  // 判断 是不是element类型
-  // processElement(vnode, container) 
-  processComponent(vnode, container)
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
 }
 
 function processComponent(vnode, container) {
+  console.log('processComponent执行了，vnode---->', vnode)
+
   mountComponent(vnode, container)
 }
+
+function processElement(vnode, container) {
+  console.log('processElement执行了，vnode---->', vnode)
+  // init -> update
+  mountElement(vnode, container)
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+  // console.log(vnode)
+  // console.log(container)
+  // 处理children
+  const { children } = vnode
+
+  /**
+   * 1. 如果是字符串，那么就是文本节点
+   * 2. 如果是数组，那么就是多个子节点,递归调用patch
+   */
+  if (typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    // console.log(children)
+    mountChildren(children, el)
+  }
+
+  // props
+  const { props } = vnode
+  for (const key in props) {
+    // console.log(key)
+    if (Object.prototype.hasOwnProperty.call(props, key)) {
+      const value = props[key]
+      el.setAttribute(key, value)
+    }
+  }
+  container.appendChild(el)
+}
+
+function mountChildren(children, container) {
+  children.forEach(child => {
+    patch(child, container)
+  })
+}
+
 function mountComponent(vnode, container) {
   const instance = createComponentInstance(vnode)
+  // 初始化组件 调用setup函数，如果有就执行
   setupComponent(instance)
+  // 渲染-> 调用render函数
   setupRenderEffect(instance, container)
 }
 function setupRenderEffect(instance: any, container: any) {
   const subTree = instance.render()
+  console.log(subTree)
   // vnode -> patch
   // vnode -> element -> mountElement
   patch(subTree, container)
